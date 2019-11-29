0b8e6d633bc93272900d8dfed5bcb20599ae56b8
mandlerm <amaya.melissa@gmail.com>
2019-11-29 15:47:43 -0600

    use refs to handle state updates based on actions taken in a child component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 // import PropTypes from 'prop-types'
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = {currentEvent: "-----"} ;
-    this.update = this.update.bind(this) ;
-
+    this.state = {a: '', b:''}
   }
 
-  update(e) {
-    console.log(e)
-    this.setState({currentEvent: e.type})
-  }
+  update(e){
+    console.log(this.refs)
+    this.setState({
+      a: this.a.refs.input.value,
+      b: this.refs.b.value
+    })
 
+  }
+  render() {
+    return (
+      <div>
+        <Input
+          ref={ component => this.a = component}
+          type="text"
+          update={this.update.bind(this)}
+          /> {this.state.a}
+          <hr/>
+        <input
+          ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+          /> {this.state.b}
+      </div>
+    )
+  }
+}
+
+class Input extends React.Component {
   render() {
     return (
     <div>
-      <textarea
-      onKeyPress={this.update}
-      onCopy={this.update}
-      onPaste={this.update}
-      onCut={this.update}
-      // onClick={this.update}
-      onFocus={this.update}
-      onBlur={this.update}
-
-
-      cols="30"
-      rows="10"/>
-      <h1>{this.state.currentEvent}</h1>
+      <input ref="input" type="text" onChange={this.props.update}/>
     </div>
     )
   }
