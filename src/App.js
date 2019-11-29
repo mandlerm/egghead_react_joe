0d5221eea55405667667369fa99b124a438d73f7
mandlerm <amaya.melissa@gmail.com>
2019-11-29 13:24:44 -0600

    custom validator returning a custom function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 class App extends React.Component {
   render() {
-    return <Button>I <Heart/> React</Button>
+    return <Title text="The Hairraise"/>
   }
 }
 
-const Button = (props) => <button>{props.children} </button>
+const Title = (props) => <h1>Title: {props.text}</h1>
 
-class Heart extends React.Component {
-  render() {
-    return <span>&hearts;</span>
+Title.propTypes = {
+  text(props, propName, component) {
+    console.log(`props = ${props.text}`) ;
+    console.log(`propName = ${propName}`) ;
+    console.log(`component = ${component}`) ;
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length < 6) {
+      return new Error(`${propName} was too short`)
+    }
   }
 }
 export default App ;
