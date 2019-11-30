8adff521261b8817bf9f7fecb83a30ee9632d8b7
mandlerm <amaya.melissa@gmail.com>
2019-11-29 20:06:09 -0600

    switch to kent tutorial

--- src/App.js
+++ src/App.js
@@ -2,36 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  constructor() {
-    super() ;
-    this.state = {increasing: false}
-  }
-  update(){
-    ReactDOM.render(<App val={this.props.val+1}/>,
-      document.getElementById('root'))
-  }
 
-  componentWillReceiveProps(nextProp) {
-    this.setState({increasing: nextProp.val > this.props.val})
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    return nextProps.val % 5 === 0;
-  }
-
-  componentDidUpdate(prevProps, prevState){
-    console.log(`prevProps: ${prevProps.val}`);
-  }
-  render() {
-    console.log(this.state.increasing)
-    return (
-      <button onClick={this.update.bind(this)}>
-        {this.props.val}
-      </button>
-    )
-  }
+    render() {
+      return(
+        <div> yoyoma </div>
+      )
+    }
 }
-
-App.defaultProps = {val:0}
 
 export default App
